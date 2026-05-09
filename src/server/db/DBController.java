e8830934d65f990f24c73f716292785e31e5ae7f
yazankhwork <yazankhwork@gmail.com>
2026-05-09 17:39:32 +0300

    Add database connection and order data methods

---
+++ src/server/db/DBController.java
@@ -0,0 +1,17 @@
+package server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBController {
+	private static final String URL =
+            "jdbc:mysql://localhost:3306/gonature_db?serverTimezone=Asia/Jerusalem&useSSL=false";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "Y123456y";
+
+    public static Connection connectToDB() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
