686b7a94f796238e4a6de855615d85bedeebd42e
yazankhwork <yazankhwork@gmail.com>
2026-05-10 19:35:07 +0300

    connect client GUI with server

--- GoNature_Prototype/src/client/PrototypeClient.java
+++ GoNature_Prototype/src/client/PrototypeClient.java
@@ -1,16 +1,42 @@
 package client;
 
 import java.io.IOException;
-
+import common.Order;
+import common.ClientRequest;
+import common.ServerResponse;
 import ocsf.client.AbstractClient;
 
 public class PrototypeClient extends AbstractClient {
 	
-	public PrototypeClient(String host, int port) {
+	private ClientGUI gui;
+	public PrototypeClient(String host, int port, ClientGUI gui) {
 	    super(host, port);
+	    this.gui = gui;
 	}
+	
 	@Override
 	protected void handleMessageFromServer(Object msg) {
-	    System.out.println("Message from server: " + msg);
+	    if (msg instanceof ServerResponse) {
+	        ServerResponse response = (ServerResponse) msg;
+
+	        System.out.println("Response from server: " + response.getMessage());
+
+	        if (gui != null) {
+	            gui.showStatus("Status: " + response.getMessage());
+
+	            if (response.isSuccess() && response.getOrder() != null) {
+	                gui.displayOrder(response.getOrder());
+	            }
+	        }
+	    } else {
+	        System.out.println("Message from server: " + msg);
+	    }
+	}
+	public void sendRequest(ClientRequest request) {
+	    try {
+	        sendToServer(request);
+	    } catch (Exception e) {
+	        System.out.println("Failed to send request: " + e.getMessage());
+	    }
 	}
 }
--- GoNature_Prototype/src/server/ServerMain.java
+++ GoNature_Prototype/src/server/ServerMain.java
@@ -1,27 +1,13 @@
 package server;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import server.db.DBController;
-import server.db.OrderDBQueries;
+import javax.swing.SwingUtilities;
 
 public class ServerMain {
-	public static void main(String[] args) {
-		System.out.println("Before update:");
-		OrderDBQueries.printAllOrders();
 
-		OrderDBQueries.updateOrder(1, "2026-05-25", 6);
-
-        System.out.println("After update:");
-        OrderDBQueries.printAllOrders();
-        PrototypeServer server = new PrototypeServer(5555);
-        try {
-			server.listen();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ServerGUI gui = new ServerGUI();
+            gui.setVisible(true);
+        });
+    }
 }
