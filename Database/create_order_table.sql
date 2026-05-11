580e2781ca4e4a2f17cb946f079bd05fb05785bf
yazankhwork <yazankhwork@gmail.com>
2026-05-11 14:06:30 +0300

    added data to test

--- Database/create_order_table.sql
+++ Database/create_order_table.sql
@@ -9,3 +9,14 @@ CREATE TABLE IF NOT EXISTS `Order` (
     subscriber_id INT,
     date_of_placing_order DATE
 );
+
+INSERT INTO `Order`
+(order_number, order_date, number_of_visitors, confirmation_code, subscriber_id, date_of_placing_order)
+VALUES
+(1, '11-05-2026', 6, 123, 100, '01-05-2026')
+ON DUPLICATE KEY UPDATE
+    order_date = VALUES(order_date),
+    number_of_visitors = VALUES(number_of_visitors),
+    confirmation_code = VALUES(confirmation_code),
+    subscriber_id = VALUES(subscriber_id),
+    date_of_placing_order = VALUES(date_of_placing_order);
